81ebef0abfd190f64db13e80ecc4f0c801833c16
Luiz Fabiano Cruz <luiz.fabiano@gpnet.com.br>
2021-06-28 09:06:26 -0300

    add swagger

--- src/main/java/com/fabianocruz/customerapi/config/SecurityConfig.java
+++ src/main/java/com/fabianocruz/customerapi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.fabianocruz.customerapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,22 +16,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthenticationEntryPoint authEntryPoint;
 
+	private static final String[] ENDPOINTS_LIBERADOS = {"/actuator/**", "/h2-console/**", "/v2/**", "/webjars/**", "/swagger-resources/**", "/swagger-ui.html"};
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-				.anyRequest().authenticated()
-				.and().httpBasic()
-				.authenticationEntryPoint(authEntryPoint);
+		http.headers().frameOptions().disable();
+		http.antMatcher("/**")
+		.authorizeRequests()
+		.antMatchers(ENDPOINTS_LIBERADOS).permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()
+		.authenticationEntryPoint(authEntryPoint);
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("user").password(encoder().encode("password")).roles("USER");
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder encoder() {
-	    return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/com/fabianocruz/customerapi/config/WebMvcConfig.java
+++ src/main/java/com/fabianocruz/customerapi/config/WebMvcConfig.java
@@ -1,13 +1,63 @@
 package com.fabianocruz.customerapi.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     
+	 @Bean
+	    public Docket api() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .select()
+	                .apis(RequestHandlerSelectors.basePackage("com.fabianocruz.customerapi"))
+	                .paths(PathSelectors.ant("/**"))
+	                .build()
+	                .securityContexts(Arrays.asList(securityContext()))
+	                .securitySchemes(Arrays.asList(new ApiKey("Basic", "Authorization", "header")));
+	 }
+
+	 private SecurityContext securityContext() {
+		 return SecurityContext.builder()
+				 .securityReferences(defaultAuth())
+				 .forPaths(PathSelectors.ant("/**"))     
+				 .build();
+	 }
+
+	 List<SecurityReference> defaultAuth() {
+		 return Arrays.asList(new SecurityReference("Basic", new AuthorizationScope[] {new AuthorizationScope("global", "accessEverything")}));
+	 }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
     @Bean
     public ModelMapper modelMapper() {
     	return new ModelMapper();
